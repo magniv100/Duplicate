5b0766e8a064631bd5e0f046a6d0b93ae114af41
magniv100 <origabbay53@gmail.com>
2026-03-11 10:28:28 +0200

    login done

--- src/schema/schema_management.py
+++ src/schema/schema_management.py
@@ -1,10 +1,11 @@
-import http
+import http, logging
 
 
 def check_object_details(metadata: dict) -> bool | http.HTTPStatus:
     for value in metadata["object_details"]:
         if value is None:
             return http.HTTPStatus.UNPROCESSABLE_CONTENT
+    logging.debug(f"Object details: {metadata}")
     return True
 
 
@@ -12,6 +13,7 @@ def check_digital_capture(metadata: dict) -> bool | http.HTTPStatus:
     for value in metadata["digital_capture"]:
         if value is None:
             return http.HTTPStatus.UNPROCESSABLE_CONTENT
+    logging.debug(f"Digital capture: {metadata}")
     return True
 
 
@@ -21,12 +23,14 @@ def check_place(metadata: dict) -> bool | http.HTTPStatus:
     for value in metadata[place]:
         if value is None:
             return http.HTTPStatus.UNPROCESSABLE_CONTENT
+    logging.debug(f"Place: {metadata}")
     return True
 
 
 def check_all_exist(metadata: dict) -> bool | http.HTTPStatus:
     if metadata["asset_id"] is not None and metadata["asset_type"] is not None:
         if check_object_details(metadata) and check_digital_capture(metadata) and check_place(metadata):
+            logging.debug(f"Existing asset: {metadata}")
             return True
         else:
             return http.HTTPStatus.UNPROCESSABLE_CONTENT
@@ -35,6 +39,7 @@ def check_all_exist(metadata: dict) -> bool | http.HTTPStatus:
 
 
 def main(metadata: dict) -> dict | http.HTTPStatus:
+    logging.debug(f"Metadata in {metadata}")
     if check_all_exist(metadata):
         return metadata
     return http.HTTPStatus.UNPROCESSABLE_CONTENT
