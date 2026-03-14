805f52f9022315e1a0d95903866f1310c65f3e19
Liat Afek <liat12afek@gmail.com>
2026-03-14 08:49:43 -0700

    added logs

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app.include_router(main_router)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
-    logging.
+    logging.log(20,"listening...")
--- src/router.py
+++ src/router.py
@@ -4,11 +4,13 @@ from fastapi import APIRouter, UploadFile, File, HTTPException
 
 from src.actions import insert_image
 from src.models.requests import InsertRequest
+import logging
 
 main_router = APIRouter()
 
 @main_router.post("/upload-image/")
 async def upload_image(image: Annotated[UploadFile, File(...)], insert_req: InsertRequest):
     if image.content_type not in ["image/jpeg", "image/peg", "image/tiff", "image/webp", "image/avif"]:
+        logging.log(10, f"image type not allowed - {image.content_type}")
         raise HTTPException(status_code=400, detail="Invalid file type. Only PEG, TIFF, AVIF, and WEBP allowed.")
     return insert_image(insert_req, image)
